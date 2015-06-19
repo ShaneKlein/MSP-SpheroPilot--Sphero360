397a84a5b16d8dc3a2233016560cd44579d11692
Shane Klein <sakp5f@mst.edu>
2015-06-19 16:51:13 -0500

    Change style

--- frontend/Sphero 360 Frontend/Sphero 360 Frontend/MainForm.Designer.cs
+++ frontend/Sphero 360 Frontend/Sphero 360 Frontend/MainForm.Designer.cs
@@ -65,9 +65,12 @@
             // 
             // _btnConnectToBackend
             // 
-            this._btnConnectToBackend.Location = new System.Drawing.Point(12, 12);
+            this._btnConnectToBackend.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("_btnConnectToBackend.BackgroundImage")));
+            this._btnConnectToBackend.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._btnConnectToBackend.ForeColor = System.Drawing.Color.White;
+            this._btnConnectToBackend.Location = new System.Drawing.Point(14, 12);
             this._btnConnectToBackend.Name = "_btnConnectToBackend";
-            this._btnConnectToBackend.Size = new System.Drawing.Size(260, 23);
+            this._btnConnectToBackend.Size = new System.Drawing.Size(303, 23);
             this._btnConnectToBackend.TabIndex = 1;
             this._btnConnectToBackend.Text = "Connect to Backend";
             this._btnConnectToBackend.UseVisualStyleBackColor = true;
@@ -75,33 +78,43 @@
             // 
             // _txtServer
             // 
-            this._txtServer.Location = new System.Drawing.Point(58, 70);
+            this._txtServer.BackColor = System.Drawing.Color.LightCyan;
+            this._txtServer.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._txtServer.Location = new System.Drawing.Point(77, 70);
             this._txtServer.Name = "_txtServer";
-            this._txtServer.Size = new System.Drawing.Size(214, 20);
+            this._txtServer.Size = new System.Drawing.Size(240, 21);
             this._txtServer.TabIndex = 2;
             this._txtServer.Text = "127.0.0.1";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(14, 72);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(16, 72);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.Size = new System.Drawing.Size(46, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "Server";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(14, 98);
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(16, 98);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.Size = new System.Drawing.Size(30, 13);
             this.label2.TabIndex = 5;
             this.label2.Text = "Port";
             // 
             // _txtPort
             // 
-            this._txtPort.Location = new System.Drawing.Point(58, 96);
+            this._txtPort.BackColor = System.Drawing.Color.LightCyan;
+            this._txtPort.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._txtPort.Location = new System.Drawing.Point(77, 96);
             this._txtPort.Maximum = new decimal(new int[] {
             9000,
             0,
@@ -113,7 +126,7 @@
             0,
             0});
             this._txtPort.Name = "_txtPort";
-            this._txtPort.Size = new System.Drawing.Size(51, 20);
+            this._txtPort.Size = new System.Drawing.Size(59, 21);
             this._txtPort.TabIndex = 6;
             this._txtPort.Value = new decimal(new int[] {
             9000,
@@ -123,7 +136,9 @@
             // 
             // _txtMaxSpeed
             // 
-            this._txtMaxSpeed.Location = new System.Drawing.Point(88, 193);
+            this._txtMaxSpeed.BackColor = System.Drawing.Color.LightCyan;
+            this._txtMaxSpeed.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._txtMaxSpeed.Location = new System.Drawing.Point(115, 160);
             this._txtMaxSpeed.Maximum = new decimal(new int[] {
             200,
             0,
@@ -135,7 +150,7 @@
             0,
             0});
             this._txtMaxSpeed.Name = "_txtMaxSpeed";
-            this._txtMaxSpeed.Size = new System.Drawing.Size(51, 20);
+            this._txtMaxSpeed.Size = new System.Drawing.Size(59, 21);
             this._txtMaxSpeed.TabIndex = 8;
             this._txtMaxSpeed.Value = new decimal(new int[] {
             75,
@@ -146,18 +161,24 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(14, 195);
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(16, 162);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.Size = new System.Drawing.Size(70, 13);
             this.label3.TabIndex = 7;
             this.label3.Text = "Max Speed";
             // 
             // _btnDisconnectFromBackend
             // 
+            this._btnDisconnectFromBackend.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("_btnDisconnectFromBackend.BackgroundImage")));
             this._btnDisconnectFromBackend.Enabled = false;
-            this._btnDisconnectFromBackend.Location = new System.Drawing.Point(12, 41);
+            this._btnDisconnectFromBackend.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._btnDisconnectFromBackend.ForeColor = System.Drawing.Color.White;
+            this._btnDisconnectFromBackend.Location = new System.Drawing.Point(14, 41);
             this._btnDisconnectFromBackend.Name = "_btnDisconnectFromBackend";
-            this._btnDisconnectFromBackend.Size = new System.Drawing.Size(260, 23);
+            this._btnDisconnectFromBackend.Size = new System.Drawing.Size(303, 23);
             this._btnDisconnectFromBackend.TabIndex = 9;
             this._btnDisconnectFromBackend.Text = "Disconnect from Backend";
             this._btnDisconnectFromBackend.UseVisualStyleBackColor = true;
@@ -165,7 +186,9 @@
             // 
             // _txtTurnRate
             // 
-            this._txtTurnRate.Location = new System.Drawing.Point(88, 245);
+            this._txtTurnRate.BackColor = System.Drawing.Color.LightCyan;
+            this._txtTurnRate.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._txtTurnRate.Location = new System.Drawing.Point(115, 212);
             this._txtTurnRate.Maximum = new decimal(new int[] {
             90,
             0,
@@ -177,7 +200,7 @@
             0,
             0});
             this._txtTurnRate.Name = "_txtTurnRate";
-            this._txtTurnRate.Size = new System.Drawing.Size(51, 20);
+            this._txtTurnRate.Size = new System.Drawing.Size(59, 21);
             this._txtTurnRate.TabIndex = 11;
             this._txtTurnRate.Value = new decimal(new int[] {
             25,
@@ -188,15 +211,20 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(14, 248);
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(16, 215);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.Size = new System.Drawing.Size(63, 13);
             this.label4.TabIndex = 10;
             this.label4.Text = "Turn Rate";
             // 
             // _txtBoostSpeed
             // 
-            this._txtBoostSpeed.Location = new System.Drawing.Point(88, 219);
+            this._txtBoostSpeed.BackColor = System.Drawing.Color.LightCyan;
+            this._txtBoostSpeed.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._txtBoostSpeed.Location = new System.Drawing.Point(115, 186);
             this._txtBoostSpeed.Maximum = new decimal(new int[] {
             200,
             0,
@@ -208,7 +236,7 @@
             0,
             0});
             this._txtBoostSpeed.Name = "_txtBoostSpeed";
-            this._txtBoostSpeed.Size = new System.Drawing.Size(51, 20);
+            this._txtBoostSpeed.Size = new System.Drawing.Size(59, 21);
             this._txtBoostSpeed.TabIndex = 13;
             this._txtBoostSpeed.Value = new decimal(new int[] {
             200,
@@ -219,9 +247,12 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(14, 221);
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(16, 188);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(68, 13);
+            this.label5.Size = new System.Drawing.Size(79, 13);
             this.label5.TabIndex = 12;
             this.label5.Text = "Boost Speed";
             // 
@@ -231,9 +262,12 @@
             // 
             // _btnShowColorDialog
             // 
-            this._btnShowColorDialog.Location = new System.Drawing.Point(17, 163);
+            this._btnShowColorDialog.BackgroundImage = global::Sphero_360_Frontend.Properties.Resources.button_tall;
+            this._btnShowColorDialog.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._btnShowColorDialog.ForeColor = System.Drawing.Color.White;
+            this._btnShowColorDialog.Location = new System.Drawing.Point(178, 160);
             this._btnShowColorDialog.Name = "_btnShowColorDialog";
-            this._btnShowColorDialog.Size = new System.Drawing.Size(255, 23);
+            this._btnShowColorDialog.Size = new System.Drawing.Size(139, 73);
             this._btnShowColorDialog.TabIndex = 14;
             this._btnShowColorDialog.Text = "Choose Sphero Color";
             this._btnShowColorDialog.UseVisualStyleBackColor = true;
@@ -241,9 +275,10 @@
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 285);
+            this.BackgroundImage = global::Sphero_360_Frontend.Properties.Resources.bg;
+            this.ClientSize = new System.Drawing.Size(331, 244);
             this.Controls.Add(this._btnShowColorDialog);
             this.Controls.Add(this._txtBoostSpeed);
             this.Controls.Add(this.label5);
@@ -257,6 +292,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this._txtServer);
             this.Controls.Add(this._btnConnectToBackend);
+            this.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.Text = "Sphero360 Frontend";
